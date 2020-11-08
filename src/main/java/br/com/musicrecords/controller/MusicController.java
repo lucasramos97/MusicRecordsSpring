2a7f97759077f5f35442047face2368f1fb53cc2
lucasramos97 <lucasramosdev@gmail.com>
2020-11-08 14:51:45 -0300

    Added API support to register songs

--- src/main/java/br/com/musicrecords/controller/MusicController.java
+++ src/main/java/br/com/musicrecords/controller/MusicController.java
@@ -3,8 +3,12 @@ package br.com.musicrecords.controller;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import br.com.musicrecords.model.Music;
@@ -21,6 +25,16 @@ public class MusicController {
   @GetMapping
   public List<Music> getMusics() {
     return musicRepository.findAll(Sort.by("artist", "title"));
+  }
+
+  @PostMapping
+  public ResponseEntity<Music> save(@RequestBody Music music) {
+    try {
+      this.musicRepository.save(music);
+      return new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
   }
 
 }
