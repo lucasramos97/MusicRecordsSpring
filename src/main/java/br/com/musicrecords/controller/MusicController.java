ee0c0b85a7114c9aead12a8a2f0ad186c5b6fc00
lucasramos97 <lucasramosdev@gmail.com>
2020-11-22 16:03:27 -0300

    Finished put endpoint

--- src/main/java/br/com/musicrecords/controller/MusicController.java
+++ src/main/java/br/com/musicrecords/controller/MusicController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -34,6 +35,16 @@ public class MusicController {
     try {
       this.musicRepository.save(music);
       return new ResponseEntity<>(HttpStatus.CREATED);
+    } catch (Exception e) {
+      return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
+    }
+  }
+
+  @PutMapping
+  public ResponseEntity<Music> edit(@RequestBody Music music) {
+    try {
+      this.musicRepository.save(music);
+      return new ResponseEntity<>(HttpStatus.OK);
     } catch (Exception e) {
       return new ResponseEntity<>(HttpStatus.BAD_REQUEST);
     }
