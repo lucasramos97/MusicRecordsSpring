5ee38f60dc04a2da510cb3f8977c89fd2a2939a3
lucasramos97 <lucasramosdev@gmail.com>
2020-12-29 17:43:46 -0300

    Changing the token expiration time to 24 hours

--- src/main/java/br/com/musicrecords/service/JwtTokenService.java
+++ src/main/java/br/com/musicrecords/service/JwtTokenService.java
@@ -14,7 +14,7 @@ import io.jsonwebtoken.SignatureAlgorithm;
 @Service
 public class JwtTokenService {
 
-  public static final long JWT_TOKEN_VALIDITY = 5 * 60 * 60;
+  public static final long JWT_TOKEN_VALIDITY = 24 * 60 * 60;
 
   @Value("${jwt.secret}")
   private String secret;
